ddc9c9987a0b6f0721c23b65f357ee55c333ddd0
6mropis9 <6mropis9@gmail.com>
2025-02-14 17:25:36 +0300

    Original Document
    The starting phase

---
+++ variables.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $name="ADRINE";
+    $age=24;
+echo "There was a man called $name.<br>";
+echo "He was $age years old.<br>";
+$name="OPIS";
+$age="30";
+echo "$name did not like his name.<br>";
+echo "But liked his $age.<br>";
+    ?>
+</body>
+</html>
